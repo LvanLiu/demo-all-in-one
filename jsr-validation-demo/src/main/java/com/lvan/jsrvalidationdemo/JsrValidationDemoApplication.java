eba9895582fb71d6a14b2f5448500e20884feb56
lvan <155333767@qq.com>
2021-10-23 12:24:53 +0800

    更新demo

---
+++ jsr-validation-demo/src/main/java/com/lvan/jsrvalidationdemo/JsrValidationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lvan.jsrvalidationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsrValidationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsrValidationDemoApplication.class, args);
+    }
+
+}
---
+++ jsr-validation-demo/src/test/java/com/lvan/jsrvalidationdemo/JsrValidationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lvan.jsrvalidationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsrValidationDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
